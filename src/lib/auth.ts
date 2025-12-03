6472c09440a360b351e3e03a4078d36522226a06
David Brito <39559632+davbrito@users.noreply.github.com>
2025-12-02 23:41:44 -0400

    feat: implement authentication models and routes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,13 @@
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { db } from "./db";
+import { passkey } from "@better-auth/passkey";
 
-export const auth = betterAuth({});
+export const auth = betterAuth({
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+    transaction: true,
+    usePlural: true,
+  }),
+  plugins: [passkey()],
+});
