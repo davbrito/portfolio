f954a09543d17275d983d3ef4bdf96d7b6277a87
David Brito <39559632+davbrito@users.noreply.github.com>
2025-12-12 20:52:28 -0400

    fix

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -7,28 +7,27 @@ export interface ExperienceData {
   highlights: string[];
 }
 
-export const getPortfolioData = async () =>
-  import("./" + ".env.data.ts")
-    .then((module) => module.data)
-    .catch((): typeof import("./.env.data.ts").data => ({
-      socialLinks: [],
-      experience: [],
-      technologies: [],
-      profile: {
-        name: "Placeholder Name",
-        title: "Developer",
-        aboutImage: "https://placehold.co/300",
-        aboutImageAlt: "Placeholder Image",
-        location: "Unknown",
-        experience: "0 years",
-        description: "No description available.",
-        brief: "No brief available.",
-        aboutParagraphs: [
-          "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        ],
-      },
-    }));
+export const getPortfolioData = async (): Promise<
+  typeof import("./.env.data.ts").data
+> => ({
+  socialLinks: [],
+  experience: [],
+  technologies: [],
+  profile: {
+    name: "Placeholder Name",
+    title: "Developer",
+    aboutImage: "https://placehold.co/300",
+    aboutImageAlt: "Placeholder Image",
+    location: "Unknown",
+    experience: "0 years",
+    description: "No description available.",
+    brief: "No brief available.",
+    aboutParagraphs: [
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    ],
+  },
+});
 
 type PortfolioData = Awaited<ReturnType<typeof getPortfolioData>>;
 
