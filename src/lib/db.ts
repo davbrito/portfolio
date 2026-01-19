610455e915828a4a1e59506f48ebbe8697cbeb5d
David Brito <39559632+davbrito@users.noreply.github.com>
2026-01-19 02:56:37 -0400

    refactor: enhance database adapter configuration with SSL support

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,15 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { POSTGRES_URL } from "astro:env/server";
 import { PrismaClient } from "../../prisma/generated/client";
 
-const adapter = new PrismaPg({ connectionString: POSTGRES_URL });
+const adapter = new PrismaPg({
+  connectionString: POSTGRES_URL,
+  ssl: import.meta.env.PROD
+    ? {
+        rejectUnauthorized: true,
+        ca: import.meta.env.DATABASE_SSL_CERT!,
+      }
+    : false,
+});
 const db = new PrismaClient({ adapter });
 
 export { db };
