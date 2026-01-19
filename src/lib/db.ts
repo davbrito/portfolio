302733de1b59da66f0e064c7e19d89f73774e9df
David Brito <39559632+davbrito@users.noreply.github.com>
2026-01-19 02:39:35 -0400

    refactor: update database connection string to use POSTGRES_URL

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { POSTGRES_PRISMA_URL } from "astro:env/server";
+import { POSTGRES_URL } from "astro:env/server";
 import { PrismaClient } from "../../prisma/generated/client";
 
-const adapter = new PrismaPg({ connectionString: POSTGRES_PRISMA_URL });
+const adapter = new PrismaPg({ connectionString: POSTGRES_URL });
 const db = new PrismaClient({ adapter });
 
 export { db };
