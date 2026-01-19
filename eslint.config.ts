1de39f7fc18a82f49b832ba521e489b97f2a669d
David Brito <39559632+davbrito@users.noreply.github.com>
2026-01-19 14:57:58 -0400

    chore: update ESLint configuration and dependencies
    - Added eslint-plugin-astro to ESLint configuration for Astro file support.
    - Updated ESLint config to include global ignores for specific directories.
    - Enhanced ESLint rules for TypeScript and React, including specific rules for Astro files.
    - Updated package.json to include @typescript-eslint/parser and eslint-plugin-astro with their respective versions.
    - Updated pnpm-lock.yaml to reflect changes in dependencies and their versions.

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,28 +1,38 @@
 import js from "@eslint/js";
 import query from "@tanstack/eslint-plugin-query";
+import eslintPluginAstro from "eslint-plugin-astro";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default defineConfig([
+export default defineConfig(
+  globalIgnores(["node_modules/", "dist/", "build/", ".astro/", "prisma/", ".vercel/"]),
+
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    settings: {
+      react: { version: "detect" },
+    },
+    extends: [
+      pluginReact.configs.flat.recommended,
+      pluginReact.configs.flat["jsx-runtime"],
+      pluginReactHooks.configs.flat["recommended-latest"],
+      query.configs["flat/recommended"],
+    ],
+    rules: {
+      "react/jsx-no-leaked-render": "warn",
+    },
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat["jsx-runtime"],
-  pluginReactHooks.configs.flat["recommended-latest"],
-  query.configs["flat/recommended"],
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/consistent-type-imports": "warn",
-      "react/jsx-no-leaked-render": "warn",
     },
   },
   {
@@ -30,6 +40,21 @@ export default defineConfig([
     files: ["src/components/ui/**/*.{ts,tsx}"],
     rules: {
       "@typescript-eslint/consistent-type-imports": "off",
+      "react/jsx-no-leaked-render": "off",
     },
   },
-]);
+  {
+    name: "astro files",
+    files: ["**/*.astro"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "Props" }],
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          globalReturn: true,
+        },
+      },
+    },
+  },
+);
