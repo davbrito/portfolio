4882b5dce4c07043a9f9d78db641bcd4d2e12cb0
David Brito <39559632+davbrito@users.noreply.github.com>
2026-05-28 16:11:03 +0000

    feat: ajustar retrasos de animación en el componente Hero para mejorar la presentación

--- src/components/pages/landing/hero.tsx
+++ src/components/pages/landing/hero.tsx
@@ -12,18 +12,26 @@ interface HeroProps {
 export function Hero({ socialLinks, profile }: HeroProps) {
   return (
     <div className="min-h-[65vh] content-center space-y-6">
-      <p className="text-primary animate-fade-in-up font-mono text-sm font-semibold [animation-delay:50ms]">
+      <p className="text-primary animate-fade-in-up font-mono text-sm font-semibold"
+      style={{ animationDelay: "100ms" }}
+      >
         Hola, mi nombre es
       </p>
-      <div className="animate-fade-in-up space-y-3 [animation-delay:150ms]">
-        <h1 className="from-foreground to-primary/70 bg-linear-to-br bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl md:text-6xl">
+      <div className="space-y-3">
+        <h1
+          className="animate-fade-in-up from-foreground to-primary/70 bg-linear-to-br bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl md:text-6xl"
+          style={{ animationDelay: "200ms" }}
+        >
           {profile.name}.
         </h1>
-        <h2 className="text-muted-foreground text-3xl font-extrabold sm:text-4xl">{profile.description}.</h2>
+        <h2 className="animate-fade-in-up text-muted-foreground text-3xl font-extrabold sm:text-4xl"
+          style={{ animationDelay: "300ms" }}>
+          {profile.description}.
+        </h2>
       </div>
-      <p className="text-muted-foreground animate-fade-in-up max-w-2xl [animation-delay:250ms]">{profile.brief}</p>
+      <p className="text-muted-foreground animate-fade-in-up max-w-2xl" style={{ animationDelay: "500ms" }}>{profile.brief}</p>
 
-      <div className="animate-fade-in-up space-x-4 [animation-delay:350ms]">
+      <div className="animate-fade-in-up space-x-4" style={{ animationDelay: "700ms" }}>
         <Button nativeButton={false} render={<a href="#proyectos" />} className="min-w-40">
           Ver Proyectos
           <ArrowRightIcon className="h-4 w-4" />
@@ -35,7 +43,7 @@ export function Hero({ socialLinks, profile }: HeroProps) {
 
       <div
         className="text-muted-foreground animate-fade-in-up flex items-center gap-4"
-        style={{ animationDelay: "450ms" }}
+        style={{ animationDelay: "800ms" }}
       >
         {socialLinks.map((link) => (
           <a
